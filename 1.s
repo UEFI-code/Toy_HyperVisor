2edbbddd9d6f8b9a1a4a6646f3a6897a91fd2188
UEFI-code <zhuoshizhang@hotmail.com>
2026-01-31 18:47:59 +0900

    Add lldb demo

--- 1.s
+++ 1.s
@@ -2,5 +2,20 @@
 .global efi_main
 
 efi_main:
-    mov rax, 0      # EFI_SUCCESS
+    #mov dx, 0x2333
+    #in al, dx
+    #int 3
+
+    mov rax, 0x08000000
+    xor rbx, rbx
+    mov [rax], rbx
+
+    loop:
+    nop
+    nop
+    nop
+    nop
+    jmp loop # attach gdb or JTAG here
+
+    mov rax, 0 # EFI_SUCCESS
     ret
